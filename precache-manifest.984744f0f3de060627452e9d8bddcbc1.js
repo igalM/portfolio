afd2c9a17ead7ea13071ede01c42c63b4c4cea50
igalM <igalmm@gmail.com>
2020-07-24 22:09:43 +0300

    Updates

---
+++ precache-manifest.984744f0f3de060627452e9d8bddcbc1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c168a2cc9a18532666c9801ce908860",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "017747a3954290b2d9ab",
+    "url": "/portfolio/static/css/main.86eb7188.chunk.css"
+  },
+  {
+    "revision": "00f9a3f1e242ae29b629",
+    "url": "/portfolio/static/js/2.1dfe5a84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.1dfe5a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017747a3954290b2d9ab",
+    "url": "/portfolio/static/js/main.faf177f9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "e23d23a50c109f44b5f79a244da5751b",
+    "url": "/portfolio/static/media/covid.e23d23a5.png"
+  },
+  {
+    "revision": "7b74d013f1da1c283fbdb566b26b2c48",
+    "url": "/portfolio/static/media/dinamo.7b74d013.png"
+  },
+  {
+    "revision": "530031be326bb079e0b5150cad1b7af8",
+    "url": "/portfolio/static/media/facebook.530031be.svg"
+  },
+  {
+    "revision": "02a1cf68a5fa3c5c40b1e5c1903b147c",
+    "url": "/portfolio/static/media/github.02a1cf68.svg"
+  },
+  {
+    "revision": "7fd59651339dbaef31a32f5f1d0bd11f",
+    "url": "/portfolio/static/media/linkedin.7fd59651.svg"
+  },
+  {
+    "revision": "7060e9f3298f4b402c25831239f23611",
+    "url": "/portfolio/static/media/profile-picture.7060e9f3.jpg"
+  },
+  {
+    "revision": "ad872bcdde05bd0cf1242c572b395d63",
+    "url": "/portfolio/static/media/school.ad872bcd.png"
+  },
+  {
+    "revision": "708b7a4739f9e065c572f77d3630800b",
+    "url": "/portfolio/static/media/weather.708b7a47.png"
+  }
+]);
