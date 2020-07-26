cdb5ee07cdb707d806bf2ee128ba844bd953406a
igalM <igalmm@gmail.com>
2020-07-26 16:09:25 +0300

    ..

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,8 @@ const Header = () => (
         <img className={styles.Profile} src={profilePic} alt="Profile picture" />
         <h1>Hi, I'm Igal Mininberg. Nice to meet you!</h1>
         <h3>I'm a <span style={{ fontWeight: "bold" }}>Full Stack Web Developer</span> currently looking for my next adventure.</h3>
-        <h3>I write beautiful code, I love what I do and all I want is to learn more.</h3>
-        <h3>Here are a few of my recent personal projects.</h3>
+        <h3>I make sure that my code is efficient and easy to read. I love to expand my knowledge and all I want is to learn more.</h3>
+        <h3>Here are some of my recent personal projects.</h3>
         <h3>Check them out!</h3>
     </div>
 );
